1f3477a39e18fb2a388b36f39e00acb9bd1c7d01
Joalbert <joalbertgonzalez@gmail.com>
2020-06-08 17:44:26 -0500

    [ADD]🚀 mongoose

--- src/server/app.js
+++ src/server/app.js
@@ -1,60 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const { PORT, APPLICATION_NAME } = require('../config');
+const { PORT, APPLICATION_NAME, MONGO_URI } = require('../config');
 
 //const PORT = 8080;
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(require('../routes').app);
 
 app.get('/', function (req, res) {
   res.json('Hello World');
 });
 
-app.get('/users', function (req, res) {
-  res.json({users: [
-    {id: 123, name: "Joalbert"},
-    {id: 321, name: "Lisset"}
-  ]});
-});
-
-app.get('/users/:id', function (req, res) {
-  const { id } = req.params;
-  res.json(`user: ${id}`);
-});
-
-app.post('/users', function (req, res) {
-  const body = req.body;
-
-  if (body.name) {
-    res.json(body);
-  } else {
-    res.status(400).json({
-      cod: 400,
-      message: 'Name is empty',
-      error: 'send name'
-    });
-  }
-});
-
-app.put('/users/:id', function (req, res) {
-  const user = {
-    id: 123,
-    name: 'Jolabert'
-  };
-  const { name } = req.body;
-  user.name = name;
-  res.json(user);
-});
-
-app.delete('/delete', function (req, res) {
-  res.json('Hello World');
-});
+mongoose.connect(`${MONGO_URI}/mydb`,
+  { useNewUrlParser: true , useUnifiedTopology: true }, error => {
+    if (error) throw error.message;
+    console.info('DB Online!!');
+  });
 
 app.listen(PORT, () => console.log(
 `App: ${APPLICATION_NAME}
